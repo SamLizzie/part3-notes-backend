58a772ce215a85a24d50abc03c03faee5f449e12
SamLizzie30 <toinga.samantha@gmail.com>
2024-09-30 12:27:53 +0200

    update backend to get data from db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Note = require('./models/note')
+
 
 const app = express();
 
@@ -26,57 +29,43 @@ let notes = [
   },
 ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes => {
+    response.json(notes);
+  }));
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
-});
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById({id: request.params.id}).then(note => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+  Note.findByIdAndDelete(request.params.id).then(result => {
+    response.status(204).end();
+  });
 });
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note ({
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId(),
-  };
+  }) ;
 
-  notes = notes.concat(note);
-
-  response.json(notes);
+  note.save().then(savedNote => {
+    response.json(savedNote);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
