c54095a05d02ca02af4ffa0a6b8b590e40374972
SamLizzie30 <toinga.samantha@gmail.com>
2024-10-07 20:36:17 +0200

    add a test only example

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,28 +1,61 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const logger = require('../utils/logger')
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  }
+]
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
-  await api.get('/api/notes').expect(200).expect('Content-Type', /application\/json/)
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
 })
 
-test('there are two notes', async () => {
+// test('there are two notes', async () => {
+//   const response = await api.get('/api/notes')
+
+//   assert.strictEqual(response.body.length, initialNotes.length)
+// })
+
+// test('there are two notes', async () => {
+//   const response = await api.get('/api/notes')
+
+//   assert.strictEqual(response.body.length, 2)
+// })
+
+// test('the first note is about HTML', async () => {
+//   const response = await api.get('/api/notes')
+
+//   const contents = response.body.map(e => e.content)
+//   assert(contents.includes('HTML is easy'))
+// })
+
+test.only('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test.only('there are two notes', async () => {
   const response = await api.get('/api/notes')
 
   assert.strictEqual(response.body.length, 2)
-})
-
-test('the first note is about Mongoose', async () => {
-  const response = await api.get('/api/notes')
-
-  const contents = response.body.map(e => e.content)
-  logger.info(contents)
-  assert(contents.includes('Mongoose makes things easy'))
 })
 
 after(async () => {
